0556be71fe7299440b11e464f4b854ec791e1f20
Luis Rovirosa <luisrovirosa@gmail.com>
2018-06-21 16:00:24 +0200

    Add gilded rose characterization testing kata

---
+++ README.md
@@ -0,0 +1,8 @@
+# Codium Working with legacy code training
+
+# Installation
+1. Download the code using GIT or with the [downloading the zip](https://github.com/CodiumTeam/legacy-training-csharp/archive/master.zip) link
+- Open with Visual Studio the solution for the current kata
+- Run the tests: Pruebas - Ejecutar - Todas las pruebas (Ctrl + R, A)
+
+# Katas
---
+++ gilded-rose-characterization-testing/KataGildedRose.Test/GildedRoseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KataGildedRose.Tests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void xxx()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Book", SellIn = 10, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal("Book", Items[0].Name);
+        }
+    }
+}
