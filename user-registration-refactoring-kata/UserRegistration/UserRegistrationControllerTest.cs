1f326aea29d44110148be497a337817d1723a1ee
Luis Rovirosa <luis@codium.team>
2020-03-03 12:37:06 +0100

    UserRegistration : add test

---
+++ user-registration-refactoring-kata/UserRegistration/UserRegistrationControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UserRegistration.Test
+{
+    public class UserRegistrationControllerTest
+    {
+        [Fact]
+        public void Post()
+        {
+           Assert.Equal(true, true);
+        }
+    }
+}
