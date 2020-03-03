fffbd7dc6dd693caf61c069504bb8a62bf36e66b
Luis Rovirosa Mairlot <luis@codium.team>
2020-03-03 17:53:09 +0100

    UserRegistration: send email

--- user-registration-refactoring-kata/UserRegistration/Controllers/UserRegistrationController.cs
+++ user-registration-refactoring-kata/UserRegistration/Controllers/UserRegistrationController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+
+
 
 namespace UserRegistration.Controllers
 {
@@ -20,22 +24,36 @@ namespace UserRegistration.Controllers
         [Consumes("application/x-www-form-urlencoded")]
         public IActionResult RegisterUser(IFormCollection FormData)
         {
-            if (FormData["password"].ToString().Length <= 8 || !FormData["password"].ToString().Contains("_")){
+            if (FormData["password"].ToString().Length <= 8 || !FormData["password"].ToString().Contains("_"))
+            {
                 return BadRequest("The password is not valid");
             }
-            if (orm.FindByEmail(FormData["email"].ToString()) != null){
+            if (orm.FindByEmail(FormData["email"].ToString()) != null)
+            {
                 return BadRequest("The email is already in use");
             }
             var rng = new Random();
             var user = new User
             {
-                Id = rng.Next(1,1000),
+                Id = rng.Next(1, 1000),
                 Name = FormData["name"],
                 Password = FormData["password"],
                 Email = FormData["email"]
             };
             orm.Save(user);
-            
+
+            SmtpClient client = new SmtpClient("mysmtpserver");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("smtpUsername", "smtpPassword");
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("noreply@codium.team");
+            mailMessage.To.Add(FormData["email"]);
+            mailMessage.Body = "This is the confirmation email";
+            mailMessage.Subject = "Welcome to Codium";
+            // If a proper SMTP server is configured, this line could be uncommented
+            //client.Send(mailMessage);
+
             return Ok(user);
         }
     }
