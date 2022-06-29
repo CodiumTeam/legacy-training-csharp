fba085831c038ad49e3d6b0b88221e1914d21e4e
Luis Rovirosa Mairlot <luis@codium.team>
2022-06-29 09:52:56 +0200

    [tennis] add braces to CalculateAdvantageOrWinScore

--- tennis-refactoring-kata/src/Tennis/TennisGame.cs
+++ tennis-refactoring-kata/src/Tennis/TennisGame.cs
@@ -56,10 +56,23 @@
         {
             string score;
             var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            if (minusResult == 1)
+            {
+                score = "Advantage player1";
+            }
+            else if (minusResult == -1)
+            {
+                score = "Advantage player2";
+            }
+            else if (minusResult >= 2)
+            {
+                score = "Win for player1";
+            }
+            else
+            {
+                score = "Win for player2";
+            }
+
             return score;
         }
 
