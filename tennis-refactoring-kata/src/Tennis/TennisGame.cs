439724f8a37aa0ea5f0eee25ffc1bdda2d9e05fc
Luis Rovirosa Mairlot <luis@codium.team>
2022-06-29 09:53:34 +0200

    [tennis] add multiple returns to CalculateAdvantageOrWinScore

--- tennis-refactoring-kata/src/Tennis/TennisGame.cs
+++ tennis-refactoring-kata/src/Tennis/TennisGame.cs
@@ -54,26 +54,23 @@
 
         private string CalculateAdvantageOrWinScore()
         {
-            string score;
             var minusResult = m_score1 - m_score2;
             if (minusResult == 1)
             {
-                score = "Advantage player1";
+                return "Advantage player1";
             }
             else if (minusResult == -1)
             {
-                score = "Advantage player2";
+                return "Advantage player2";
             }
             else if (minusResult >= 2)
             {
-                score = "Win for player1";
+                return "Win for player1";
             }
             else
             {
-                score = "Win for player2";
+                return "Win for player2";
             }
-
-            return score;
         }
 
         private bool IsAdvantageOrWin()
