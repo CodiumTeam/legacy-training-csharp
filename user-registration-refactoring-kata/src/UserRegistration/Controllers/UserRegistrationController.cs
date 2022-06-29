ccb78cd94abaf64984b7bb65c8c7d9eb5e308f52
Luis Rovirosa Mairlot <luis@codium.team>
2022-06-29 10:57:26 +0200

    [user-registration] part 1 - step 1

--- user-registration-refactoring-kata/src/UserRegistration/Controllers/UserRegistrationController.cs
+++ user-registration-refactoring-kata/src/UserRegistration/Controllers/UserRegistrationController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using UserRegistration.Domain;
 using UserRegistration.Infrastructure;
+using UserRegistration.UseCase;
 
 namespace UserRegistration.Controllers
 {
@@ -16,37 +17,7 @@ namespace UserRegistration.Controllers
         [Consumes("application/x-www-form-urlencoded")]
         public IActionResult RegisterUser(IFormCollection formData)
         {
-            if (formData["password"].ToString().Length <= 8 || !formData["password"].ToString().Contains("_"))
-            {
-                return new BadRequestObjectResult("The password is not valid");
-            }
-            if (orm.FindByEmail(formData["email"].ToString()) != null)
-            {
-                return new BadRequestObjectResult("The email is already in use");
-            }
-            var rng = new Random();
-            var user = new User
-            {
-                Id = rng.Next(1, 1000),
-                Name = formData["name"].ToString(),
-                Password = formData["password"].ToString(),
-                Email = formData["email"].ToString()
-            };
-            orm.Save(user);
-
-            SmtpClient client = new SmtpClient("mysmtpserver");
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("smtpUsername", "smtpPassword");
-
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("noreply@codium.team");
-            mailMessage.To.Add(formData["email"].ToString());
-            mailMessage.Body = "This is the confirmation email";
-            mailMessage.Subject = "Welcome to Codium";
-            // If a proper SMTP server is configured, this line could be uncommented
-            //client.Send(mailMessage);
-
-            return new OkObjectResult(user);
+            return new RegisterUser().Execute(formData);
         }
     }
 }
