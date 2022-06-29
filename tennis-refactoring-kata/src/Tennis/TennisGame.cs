a57c19abc345e910452a33f4646c74e20ce833cd
Luis Rovirosa Mairlot <luis@codium.team>
2022-06-29 09:29:41 +0200

    [tennis] extract methods from the main method

--- tennis-refactoring-kata/src/Tennis/TennisGame.cs
+++ tennis-refactoring-kata/src/Tennis/TennisGame.cs
@@ -25,57 +25,94 @@
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (HasSameScore())
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = CalculateScoreWithSameScore();
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (IsAdvantageOrWin())
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = CalculateAdvantageOrWinScore();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                score = CalculateRegularScore(score);
             }
             return score;
+        }
+
+        private string CalculateRegularScore(string score)
+        {
+            int tempScore;
+            for (var i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = m_score1;
+                else
+                {
+                    score += "-";
+                    tempScore = m_score2;
+                }
+
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
+
+            return score;
+        }
+
+        private string CalculateAdvantageOrWinScore()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private bool IsAdvantageOrWin()
+        {
+            return m_score1 >= 4 || m_score2 >= 4;
+        }
+
+        private string CalculateScoreWithSameScore()
+        {
+            string score;
+            switch (m_score1)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+
+            return score;
+        }
+
+        private bool HasSameScore()
+        {
+            return m_score1 == m_score2;
         }
     }
 }
