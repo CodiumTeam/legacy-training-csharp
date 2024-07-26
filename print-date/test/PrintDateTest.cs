28976a06cf9171963e178b7d8c842f5a970c1005
Luis Rovirosa Mairlot <luis@codium.team>
2024-07-26 11:15:24 +0200

    [print-date] solution with Nsubstitute, moq and manual test doubles

--- print-date/test/PrintDateTest.cs
+++ print-date/test/PrintDateTest.cs
@@ -1,4 +1,5 @@
-﻿using NSubstitute;
+﻿using Moq;
+using NSubstitute;
 using PrintDate;
 
 namespace KataPrintDate.Tests
@@ -6,13 +7,66 @@ namespace KataPrintDate.Tests
     public class PrintDateTest
     {
         [Fact]
-        public void think_a_good_name_and_change_it()
+        public void prints_the_current_date_with_nsubstitute()
         {
-            var printDate = new PrintDate.PrintDate(new Calendar(), new Printer());
+            var printer = Substitute.For<IPrinter>();
+            var calendar = Substitute.For<ICalendar>();
+            calendar.Today().Returns(new DateTime(2024, 01, 02));
+            var printDate = new PrintDate.PrintDate(calendar, printer);
 
             printDate.PrintCurrentDate();
 
-            // I don't know how to test it 
+            printer.Received().PrintLine("2/1/2024 0:00:00");
         }
+        
+        [Fact]
+        public void prints_the_current_date_with_moq()
+        {
+            var printer = new Mock<IPrinter>();
+            var calendar = new Mock<ICalendar>();
+            calendar.Setup(c => c.Today()).Returns(new DateTime(2024, 01, 02));
+            var printDate = new PrintDate.PrintDate(calendar.Object, printer.Object);
+
+            printDate.PrintCurrentDate();
+
+            printer.Verify(p => p.PrintLine("2/1/2024 0:00:00"));
+        }
+        
+        [Fact]
+        public void prints_the_current_date_without_library()
+        {
+            var printer = new PrinterSpy();
+            var calendar = new CalendarStub(new DateTime(2024, 01, 02));
+            var printDate = new PrintDate.PrintDate(calendar, printer);
+
+            printDate.PrintCurrentDate();
+
+            Assert.Equal("2/1/2024 0:00:00", printer.PrintedLine);
+        }
+    }
+
+    public class CalendarStub : ICalendar
+    {
+        private DateTime _dateTime;
+
+        public CalendarStub(DateTime today)
+        {
+            _dateTime = today;
+        }
+
+        public DateTime Today()
+        {
+            return _dateTime;
+        }
+    }
+
+    public class PrinterSpy : IPrinter
+    {
+        public void PrintLine(string line)
+        {
+            PrintedLine = line;
+        }
+
+        public string PrintedLine { get; set; }
     }
 }
